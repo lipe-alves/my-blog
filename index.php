8eafa14962997c7c5ab9d5259eac62855c0b024f
Felipe Alves <felipe.contato.a@gmail.com>
2024-12-12 13:12:13 -0300

    add: Add try catch in the root for debugging start-database.sql script

--- index.php
+++ index.php
@@ -7,19 +7,29 @@ use Dotenv\Dotenv;
 $dotenv = Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
-require_once "./app/config/constants.php";
-require_once "./app/helpers/index.php";
-require_once "./app/config/routes.php";
+try {
+    function start_database()
+    {
+        $sql = file_get_contents("./app/config/start-database.sql");
+        $conn = new PDO(
+            "mysql:host=$_ENV[DB_HOST]",
+            $_ENV["DB_USER"],
+            $_ENV["DB_PASSWORD"]
+        );
+        $conn->exec($sql);
+    }
 
-function start_database()
-{
-    $sql = file_get_contents("./app/config/start-database.sql");
-    $conn = new PDO(
-        "mysql:host=$_ENV[DB_HOST]",
-        $_ENV["DB_USER"],
-        $_ENV["DB_PASSWORD"]
-    );
-    $conn->exec($sql);
+    start_database();
+
+    require_once "./app/config/constants.php";
+    require_once "./app/helpers/index.php";
+    require_once "./app/config/routes.php";
+} catch (\Exception $exception) {
+    header("Content-Type: application/json");
+
+    echo json_encode([
+        "success" => false,
+        "message" => $exception->getMessage(),
+        "data"    => []
+    ]);
 }
-
-start_database();
