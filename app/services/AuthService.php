2c4444abb9405eb57103c536e8b1fcbfac728e2d
Felipe Alves <felipe.contato.a@gmail.com>
2025-01-17 17:30:50 -0300

    fix: tipagem de retorno errada

--- app/services/AuthService.php
+++ app/services/AuthService.php
@@ -9,7 +9,7 @@ class AuthService
         return password_hash($password, PASSWORD_DEFAULT);
     }
 
-    public static function verifyPassword(string $password, string $hash): string
+    public static function verifyPassword(string $password, string $hash): bool
     {
         return password_verify($password, $hash);
     }
