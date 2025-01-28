fff96c57d0bac1771cd414b7d5c601b00bac2ffd
Felipe Alves <felipe.contato.a@gmail.com>
2025-01-28 20:42:06 -0300

    add: efeito de carregamento ao autenticar-se

--- public/js/admin-modal.js
+++ public/js/admin-modal.js
@@ -2,14 +2,27 @@ $(document).ready(async function () {
     window.modal.show({
         title: "Autentique-se como administrador",
         content: `
-            <input class="input is-fullwidth" name="password" placeholder="Digite a senha...">
+            <input 
+                id="password-input"
+                class="input is-fullwidth" 
+                name="password" 
+                placeholder="Digite a senha..."
+            >
         `,
         footer: `
             <div class="buttons">
-                <button class="button" onclick="handleCancelAuth()">
+                <button 
+                    id="cancellation-btn"
+                    class="button" 
+                    onclick="handleCancelAuth()"
+                >
                     Cancelar
                 </button>
-                <button class="button is-success" onclick="handleSubmitCredentials()">
+                <button 
+                    id="authentication-btn"
+                    class="button is-success" 
+                    onclick="handleSubmitCredentials()"
+                >
                     Autenticar-se
                 </button>
             </div>
@@ -26,17 +39,35 @@ function handleCancelAuth() {
 
 async function handleSubmitCredentials() {
     const { api } = window;
-    const { clearQueryParams, setQueryParams } = window.functions;
+    const { clearQueryParams, setQueryParams, delayAsync } = window.functions;
 
-    try {
-        const password = $('[name="password"]').val();
+    const submitButton = $("#authentication-btn");
+    const cancellationButton = $("#cancellation-btn");
+    const passwordInput = $("#password-input");
+
+    /** @param {boolean} disabled */
+    const setFormDisabled = (disabled) => {
+        submitButton.prop("disabled", disabled);
+        cancellationButton.prop("disabled", disabled);
+        submitButton.toggleClass("is-loading");
+        passwordInput.prop("disabled", disabled);
+    };
+
+    const authenticate = async () => {
+        const password = passwordInput.val();
         await api.admin.authenticate(password);
+
         clearQueryParams();
         setQueryParams({ admin: true, password });
         window.location.reload();
+    };
+
+    try {
+        setFormDisabled(true);
+        await delayAsync(authenticate, 3000);
     } catch (err) {
         toast.error(err.message);
     } finally {
-        toast.error(err.message);
+        setFormDisabled(false);
     }
 }
