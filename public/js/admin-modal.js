edf2f7c404502f4630d81eeb96bc7880ef2827ad
Felipe Alves <felipe.contato.a@gmail.com>
2025-01-28 16:13:20 -0300

    add: cria lógica de login com modal de autenticação

--- public/js/admin-modal.js
+++ public/js/admin-modal.js
@@ -2,19 +2,41 @@ $(document).ready(async function () {
     window.modal.show({
         title: "Autentique-se como administrador",
         content: `
-            <div class="">
-                <input class="input is-fullwidth"
-            </div>
+            <input class="input is-fullwidth" name="password" placeholder="Digite a senha...">
         `,
         footer: `
             <div class="buttons">
                 <button class="button" onclick="handleCancelAuth()">
                     Cancelar
                 </button>
-                <button class="button is-success" onclick="handleAuth()">
+                <button class="button is-success" onclick="handleSubmitCredentials()">
                     Autenticar-se
                 </button>
             </div>
-        `
+        `,
+        onHide: handleCancelAuth
     });
 });
+
+function handleCancelAuth() {
+    const { clearQueryParams } = window.functions;
+    clearQueryParams();
+    window.location.reload();
+}
+
+async function handleSubmitCredentials() {
+    const { api } = window;
+    const { clearQueryParams, setQueryParams } = window.functions;
+
+    try {
+        const password = $('[name="password"]').val();
+        await api.admin.authenticate(password);
+        clearQueryParams();
+        setQueryParams({ admin: true, password });
+        window.location.reload();
+    } catch (err) {
+        toast.error(err.message);
+    } finally {
+        toast.error(err.message);
+    }
+}
