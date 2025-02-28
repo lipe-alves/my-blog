a50f4a4255caafc2b5b3a059f5fa7c394ecb4f86
Felipe Alves <felipe.contato.a@gmail.com>
2025-02-28 16:36:29 -0300

    refactor: Aplicação do paradigma POO na media-library

--- public/js/media-library.js
+++ public/js/media-library.js
@@ -1,30 +1,40 @@
 (() => {
-    if (!window.admin) {
-        window.admin = {};
-    }
+    class MediaLibrary {
+        #configs;
+        #id;
 
-    let mediaLibrary = {
-        configs: {
-            media_type: "image/*"
-        },
+        constructor(mediaLibraryId) {
+            this.#id = mediaLibraryId;
+            this.#configs = {
+                media_type: "image/*"
+            };
+        }
+
+        get element() {
+            return $(`#${this.#id}`)[0];
+        }
+
+        get configs() {
+            return this.#configs;
+        }
 
         async show(configs = {}) {
             const { modal } = window;
 
-            this.configs = configs;
+            this.#configs = configs;
 
             await modal.show({
                 title: "Biblioteca de Mídia",
                 view: "media-library",
                 params: configs
             });
-        },
+        }
 
         /** @param {string} path */
         async setBasePath(path) {
-            this.configs.base_path = path;
+            this.#configs.base_path = path;
             await this.reload();
-        },
+        }
 
         async reload() {
             const { api } = window;
@@ -32,23 +42,13 @@
             const element = $(this.element);
             const parent = element.parent();
 
-            await api.views.render("media-library", parent[0], this.configs);
+            await api.views.render("media-library", parent[0], this.#configs);
         }
-    };
+    }
+    
+    if (!window.admin) {
+        window.admin = {};
+    }
 
-    mediaLibrary = new Proxy(mediaLibrary, {
-        get(target, prop) {
-            if (prop === "element") return $("#media-library")[0];
-            return target[prop];
-        },
-        set(target, prop, value) {
-            if (prop === "element") {
-                throw new Error("element is readonly");
-            }
-            target[prop] = value;
-            return true;
-        }
-    });
-
-    window.admin.mediaLibrary = mediaLibrary;
+    window.admin.mediaLibrary = new MediaLibrary("media-library");
 })();
