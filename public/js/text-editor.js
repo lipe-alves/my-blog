66a3b8701a045baf16943863bb41d5c34ad0bc33
Felipe Alves <felipe.contato.a@gmail.com>
2025-02-28 16:28:40 -0300

    refactor: define classes dentro de escopos fechados

--- public/js/text-editor.js
+++ public/js/text-editor.js
@@ -1,85 +1,87 @@
-class TextEditor extends Quill {
-    /** @property {string} old */
-    old;
+(() => {
+    class TextEditor extends Quill {
+        /** @property {string} old */
+        old;
 
-    /** @param {string} selector */
-    constructor(selector) {
-        super(selector, { ...TextEditor.EDITOR_CONFIG });
-        this.old = this.value;
-    }
+        /** @param {string} selector */
+        constructor(selector) {
+            super(selector, { ...TextEditor.EDITOR_CONFIG });
+            this.old = this.value;
+        }
 
-    static get EDITOR_CONFIG() {
-        return {
-            modules: {
-                toolbar: {
-                    container: [
-                        ["bold", "italic", "underline", "strike"],
-                        ["blockquote", "code-block"],
-                        ["link", "image", "video", "formula"],
+        static get EDITOR_CONFIG() {
+            return {
+                modules: {
+                    toolbar: {
+                        container: [
+                            ["bold", "italic", "underline", "strike"],
+                            ["blockquote", "code-block"],
+                            ["link", "image", "video", "formula"],
 
-                        [{ "list": "ordered" }, { "list": "bullet" }, { "list": "check" }],
-                        [{ "script": "sub" }, { "script": "super" }],
+                            [{ "list": "ordered" }, { "list": "bullet" }, { "list": "check" }],
+                            [{ "script": "sub" }, { "script": "super" }],
 
-                        [{ "indent": "-1" }, { "indent": "+1" }],
-                        [{ "direction": "rtl" }],
+                            [{ "indent": "-1" }, { "indent": "+1" }],
+                            [{ "direction": "rtl" }],
 
-                        [{ "size": ["small", false, "large", "huge"] }],
-                        [{ "header": [1, 2, 3, 4, 5, 6, false] }],
+                            [{ "size": ["small", false, "large", "huge"] }],
+                            [{ "header": [1, 2, 3, 4, 5, 6, false] }],
 
-                        [{ "color": [] }, { "background": [] }],
-                        [{ "font": [] }],
-                        [{ "align": [] }],
+                            [{ "color": [] }, { "background": [] }],
+                            [{ "font": [] }],
+                            [{ "align": [] }],
 
-                        ["clean"]
-                    ],
-                    handlers: {
-                        image: TextEditor.toolbarHandler(TextEditor.uploadImage)
+                            ["clean"]
+                        ],
+                        handlers: {
+                            image: TextEditor.toolbarHandler(TextEditor.uploadImage)
+                        }
                     }
-                }
-            },
-            theme: "snow"
-        };
+                },
+                theme: "snow"
+            };
+        }
+
+        /** @param {(editor: TextEditor) => void} handler */
+        static toolbarHandler(handler) {
+            return function () {
+                handler(this.quill);
+            };
+        }
+
+        async uploadImage() {
+            TextEditor.uploadImage(this);
+        }
+
+        /** @param {TextEditor} editor */
+        static async uploadImage(editor) {
+            const { admin } = window;
+
+            await admin.mediaLibrary.show();
+        }
+
+        get html() {
+            return this.root.innerHTML;
+        }
+
+        get value() {
+            return this.html;
+        }
+
+        /** @param {string} html */
+        set value(html) {
+            this.clipboard.dangerouslyPasteHTML(html);
+        }
+
+        get text() {
+            return this.getText();
+        }
+
+        /** @param {string} selector */
+        static create(selector) {
+            return new TextEditor(selector);
+        }
     }
 
-    /** @param {(editor: TextEditor) => void} handler */
-    static toolbarHandler(handler) {
-        return function () {
-            handler(this.quill);
-        };
-    }
-
-    async uploadImage() {
-        TextEditor.uploadImage(this);
-    }
-
-    /** @param {TextEditor} editor */
-    static async uploadImage(editor) {
-        const { admin } = window;
-
-        await admin.mediaLibrary.show();
-    }
-
-    get html() {
-        return this.root.innerHTML;
-    }
-
-    get value() {
-        return this.html;
-    }
-
-    /** @param {string} html */
-    set value(html) {
-        this.clipboard.dangerouslyPasteHTML(html);
-    }
-
-    get text() {
-        return this.getText();
-    }
-
-    /** @param {string} selector */
-    static create(selector) {
-        return new TextEditor(selector);
-    }
-}
-
-window.functions.createTextEditor = TextEditor.create;
+    window.functions.createTextEditor = TextEditor.create;
+})();
--- public/js/views.js
+++ public/js/views.js
@@ -1,96 +1,98 @@
-class View {
-    #id;
-    #loaderId;
+(() => {
+    class View {
+        #id;
+        #loaderId;
 
-    /** @param {HTMLElement} viewElement */
-    constructor(viewElement) {
-        viewElement = $(viewElement);
-        this.#id = viewElement.attr("id");
-        this.#loaderId = `loader-${this.#id}`;
-    }
+        /** @param {HTMLElement} viewElement */
+        constructor(viewElement) {
+            viewElement = $(viewElement);
+            this.#id = viewElement.attr("id");
+            this.#loaderId = `loader-${this.#id}`;
+        }
 
-    get id() {
-        return this.#id;
-    }
+        get id() {
+            return this.#id;
+        }
 
-    /** @returns {HTMLElement} */
-    get element() {
-        return $(`#${this.#id}`)[0];
-    }
+        /** @returns {HTMLElement} */
+        get element() {
+            return $(`#${this.#id}`)[0];
+        }
 
-    get loader() {
-        const view = this;
-        const loaderId = this.#loaderId;
+        get loader() {
+            const view = this;
+            const loaderId = this.#loaderId;
 
-        return {
-            show(params = {}) {
-                const { loader } = window;
-                return loader.show(view.element, { ...params, id: loaderId });
-            },
-            hide() {
-                const { loader } = window;
-                return loader.hide(loaderId);
+            return {
+                show(params = {}) {
+                    const { loader } = window;
+                    return loader.show(view.element, { ...params, id: loaderId });
+                },
+                hide() {
+                    const { loader } = window;
+                    return loader.hide(loaderId);
+                }
             }
         }
-    }
 
-    async reload(params = {}, parent = null) {
-        const { api } = window;
-        const loader = $(`#${this.#loaderId}`).clone(true);
+        async reload(params = {}, parent = null) {
+            const { api } = window;
+            const loader = $(`#${this.#loaderId}`).clone(true);
 
-        this.loader.show();
+            this.loader.show();
 
-        await api.views.reload(this.element, params, parent);
+            await api.views.reload(this.element, params, parent);
 
-        $(this.element).append(loader.prop("outerHTML"));
+            $(this.element).append(loader.prop("outerHTML"));
 
-        this.loader.hide();
-    }
-
-    static create(viewElement) {
-        const { convertToCamelCase } = window.functions;
-
-        const view = new View(viewElement);
-
-        const key = convertToCamelCase(view.id);
-        if (!window.views) {
-            window.views = {};
+            this.loader.hide();
         }
 
-        window.views[key] = view;
+        static create(viewElement) {
+            const { convertToCamelCase } = window.functions;
 
-        return view;
-    }
-}
+            const view = new View(viewElement);
 
-$(document).ready(function () {
-    $("[data-view]").each(function () {
-        View.create(this);
-    });
+            const key = convertToCamelCase(view.id);
+            if (!window.views) {
+                window.views = {};
+            }
 
-    window.views.renderNewView = async function (viewName, viewParent, params) {
-        const { api } = window;
-        const { getQueryParams } = window.functions;
+            window.views[key] = view;
 
-        if (!params) {
-            const query = getQueryParams();
-            params = query;
+            return view;
         }
+    }
+
+    $(document).ready(function () {
+        $("[data-view]").each(function () {
+            View.create(this);
+        });
+
+        window.views.renderNewView = async function (viewName, viewParent, params) {
+            const { api } = window;
+            const { getQueryParams } = window.functions;
+
+            if (!params) {
+                const query = getQueryParams();
+                params = query;
+            }
         
-        await api.views.render(viewName, viewParent, params);
+            await api.views.render(viewName, viewParent, params);
 
-        const viewElement = $(viewParent).find(`[data-view="${viewName}"]`);
-        View.create(viewElement);
-    };
+            const viewElement = $(viewParent).find(`[data-view="${viewName}"]`);
+            View.create(viewElement);
+        };
 
-    window.views.reloadAllViews = async function () {
-        const { getQueryParams } = window.functions;
-        const query = getQueryParams();
+        window.views.reloadAllViews = async function () {
+            const { getQueryParams } = window.functions;
+            const query = getQueryParams();
 
-        for (const view of Object.values(window.views)) {
-            if ("reload" in view) {
-                await view.reload(query);
+            for (const view of Object.values(window.views)) {
+                if ("reload" in view) {
+                    await view.reload(query);
+                }
             }
-        }
-    };
-});
+        };
+    });
+})();
